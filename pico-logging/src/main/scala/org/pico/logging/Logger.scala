977c3168e20d268a5b9a8fbe659683f6f97b7a31
John Ky <newhoggy@gmail.com>
2016-09-16 15:21:12 +1000

    New way of constructing Logger

--- pico-logging/src/main/scala/org/pico/logging/Logger.scala
+++ pico-logging/src/main/scala/org/pico/logging/Logger.scala
@@ -15,29 +15,29 @@ trait Logger {
 }
 
 object Logger {
-  def apply[C: ClassTag]: Logger = {
+  def apply(clazz: Class[_]): Logger = {
     new Logger {
-      val runtimeClass = implicitly[ClassTag[C]].runtimeClass
-
       override def debug(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(runtimeClass, LogDebugLevel, message, exception))
+        LogBus.publish(LogEvent(clazz, LogDebugLevel, message, exception))
       }
 
       override def trace(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(runtimeClass, LogTraceLevel, message, exception))
+        LogBus.publish(LogEvent(clazz, LogTraceLevel, message, exception))
       }
 
       override def info(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(runtimeClass, LogInfoLevel, message, exception))
+        LogBus.publish(LogEvent(clazz, LogInfoLevel, message, exception))
       }
 
       override def warn(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(runtimeClass, LogWarnLevel, message, exception))
+        LogBus.publish(LogEvent(clazz, LogWarnLevel, message, exception))
       }
 
       override def error(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(runtimeClass, LogErrorLevel, message, exception))
+        LogBus.publish(LogEvent(clazz, LogErrorLevel, message, exception))
       }
     }
   }
+
+  def apply[C: ClassTag]: Logger = this(implicitly[ClassTag[C]].runtimeClass)
 }
