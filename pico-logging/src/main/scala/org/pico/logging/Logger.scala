8597b23ea39191da48d68b04866cd455aa7bf448
John Ky <newhoggy@gmail.com>
2016-11-03 15:51:29 +1100

    Version 2.0.3

--- pico-logging/src/main/scala/org/pico/logging/Logger.scala
+++ pico-logging/src/main/scala/org/pico/logging/Logger.scala
@@ -3,38 +3,38 @@ package org.pico.logging
 import scala.reflect.ClassTag
 
 trait Logger {
-  def debug(message: => String, exception: Exception = null): Unit
+  def debug(message: => String, throwable: Throwable = null): Unit
 
-  def trace(message: => String, exception: Exception = null): Unit
+  def trace(message: => String, throwable: Throwable = null): Unit
 
-  def info(message: => String, exception: Exception = null): Unit
+  def info(message: => String, throwable: Throwable = null): Unit
 
-  def warn(message: => String, exception: Exception = null): Unit
+  def warn(message: => String, throwable: Throwable = null): Unit
 
-  def error(message: => String, exception: Exception = null): Unit
+  def error(message: => String, throwable: Throwable = null): Unit
 }
 
 object Logger {
   def apply(clazz: Class[_]): Logger = {
     new Logger {
-      override def debug(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(clazz, LogDebugLevel, message, exception))
+      override def debug(message: => String, throwable: Throwable = null): Unit = {
+        LogBus.publish(LogEvent(clazz, LogDebugLevel, message, throwable))
       }
 
-      override def trace(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(clazz, LogTraceLevel, message, exception))
+      override def trace(message: => String, throwable: Throwable = null): Unit = {
+        LogBus.publish(LogEvent(clazz, LogTraceLevel, message, throwable))
       }
 
-      override def info(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(clazz, LogInfoLevel, message, exception))
+      override def info(message: => String, throwable: Throwable = null): Unit = {
+        LogBus.publish(LogEvent(clazz, LogInfoLevel, message, throwable))
       }
 
-      override def warn(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(clazz, LogWarnLevel, message, exception))
+      override def warn(message: => String, throwable: Throwable = null): Unit = {
+        LogBus.publish(LogEvent(clazz, LogWarnLevel, message, throwable))
       }
 
-      override def error(message: => String, exception: Exception = null): Unit = {
-        LogBus.publish(LogEvent(clazz, LogErrorLevel, message, exception))
+      override def error(message: => String, throwable: Throwable = null): Unit = {
+        LogBus.publish(LogEvent(clazz, LogErrorLevel, message, throwable))
       }
     }
   }
